8d7cf2b5f7286aa1007b4a0dd889807fa73275f7
Seungwon-Choi <bestwondev@gmail.com>
2025-05-06 23:23:58 +0900

    feat: 양방향 연관관계 @JsonIgnore 추가

--- src/main/java/jpaproject/domain/OrderItem.java
+++ src/main/java/jpaproject/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpaproject.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpaproject.domain.item.Item;
 import lombok.AccessLevel;
@@ -22,6 +23,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "oreder_id")
     private Order order;
